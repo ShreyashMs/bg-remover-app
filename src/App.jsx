d471ab5eb6c662fd340d2df3684bdc79dacb86fa
ShreyashMs <145094664+ShreyashMs@users.noreply.github.com>
2024-10-25 11:39:31 +0530

    Project completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <ToastContainer position="bottom-right"/>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
